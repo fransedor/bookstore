fce33f02271a91207ba64563a1dc5c9c5d7ae807
fransedor <fransedoravelino@gmail.com>
2024-03-28 14:14:39 +0700

    init express

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
-import fs from "fs";
-import { newVar } from "./test.js";
+import express from 'express';
+import bodyParser from 'body-parser';
+import http from 'http';
+import 'dotenv/config'
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`)
+});
